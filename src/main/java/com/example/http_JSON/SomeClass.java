c8fbea464daff42c80f5d363e91d847b98651063
moreno <konradpstrongowski@gmail.com>
2023-01-31 07:52:43 +0100

    1. initial implementation - http request

---
+++ src/main/java/com/example/http_JSON/SomeClass.java
@@ -0,0 +1,25 @@
+package com.example.http_JSON;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class SomeClass {
+    private final static String BASE_URL = "http://api.open-notify.org/iss-now.json";
+
+    public static void main(String[] args) throws IOException {
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        System.out.println(response.body().string());
+    }
+
+
+
+}
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.issappsda {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
 
 
     opens com.example.issappsda to javafx.fxml;
