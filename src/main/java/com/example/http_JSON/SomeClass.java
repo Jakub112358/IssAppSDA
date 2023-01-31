bf85212d2ae628fb3d40dd11b57eaca90e5c02ea
moreno <konradpstrongowski@gmail.com>
2023-01-31 14:17:05 +0100

    2. initial JSON deserialization - simple fields

--- src/main/java/com/example/http_JSON/SomeClass.java
+++ src/main/java/com/example/http_JSON/SomeClass.java
@@ -1,25 +1,26 @@
 package com.example.http_JSON;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class SomeClass {
     private final static String BASE_URL = "http://api.open-notify.org/iss-now.json";
 
     public static void main(String[] args) throws IOException {
-        Request request = new Request.Builder()
-                .url(BASE_URL)
-                .build();
-        OkHttpClient client = new OkHttpClient();
-        Call call = client.newCall(request);
-        Response response = call.execute();
-        System.out.println(response.body().string());
+        URL url = new URL(BASE_URL);
+        JsonNode pojo = jsonDeserialization(url);
+        System.out.println(jsonDeserialization(url).toString());
+
+        int timestamp = pojo.get("timestamp").asInt();
+        double latitude = pojo.get("iss_position").get("latitude").asDouble();
+        double longitude = pojo.get("iss_position").get("longitude").asDouble();
     }
 
-
-
+    public static JsonNode jsonDeserialization(URL url) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readTree(url);
+    }
 }
-
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.issappsda {
     requires javafx.controls;
     requires javafx.fxml;
     requires okhttp3;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.example.issappsda to javafx.fxml;
