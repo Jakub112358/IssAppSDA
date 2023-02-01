0909ecf1be083606db92d3e46acd355f211c271e
Jakub112358 <jakubszymanski@hotmail.com>
2023-02-01 18:59:13 +0100

    tasks 3, 4: -added iss pass view and switching between views

--- src/main/java/iss/Controller.java
+++ src/main/java/iss/Controller.java
@@ -1,7 +1,10 @@
 package iss;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 
 public class Controller {
@@ -22,6 +25,22 @@ public class Controller {
 
     @FXML
     private Label timeLabel;
+
+    @FXML
+    private GridPane currrentInfoPane;
+
+    @FXML
+    private GridPane issPassPane;
+
+    @FXML
+    private TextField latInput;
+
+    @FXML
+    private TextField altInput;
+
+    @FXML
+    private Label nextPassLabel;
+
     @FXML
     void onRefreshButton() {
         timeLabel.setText(service.getTime());
@@ -29,6 +48,30 @@ public class Controller {
         peopleLabel.setText(service.getPeople());
         latLabel.setText(service.getLatitude());
         altLabel.setText(service.getAltitude());
+    }
+
+    @FXML
+    void onCurrentInfoButton() {
+        onRefreshButton();
+        issPassPane.setVisible(false);
+        currrentInfoPane.setVisible(true);
+    }
+
+    @FXML
+    void onIssPassButton() {
+        issPassPane.setVisible(true);
+        currrentInfoPane.setVisible(false);
+    }
+
+    @FXML
+    void onCalculateIssPass() {
+        String latInputString = latInput.getCharacters().toString();
+        String altInputString = altInput.getCharacters().toString();
+        if(service.validateInputLat(latInputString) && service.validateInputAlt(altInputString)){
+            nextPassLabel.setText("someday...");
+        } else {
+            nextPassLabel.setText("incorrect input");
+        }
     }
 
 }
