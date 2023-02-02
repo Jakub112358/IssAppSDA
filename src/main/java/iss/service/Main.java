8966af1a8455c4599c546bc9a4882226b7a31751
moreno <konradpstrongowski@gmail.com>
2023-02-02 20:02:45 +0100

    test commit

---
+++ src/main/java/iss/service/Main.java
@@ -0,0 +1,7 @@
+package iss.service;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(new Service().getSpeed());
+    }
+}
