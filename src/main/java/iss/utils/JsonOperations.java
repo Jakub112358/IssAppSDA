3ae11d8078f56199504b6948ded173a81d3df97d
moreno <konradpstrongowski@gmail.com>
2023-02-02 18:02:47 +0100

    6. SpaceCrew.java stays, Astronaut.java deleted

--- src/main/java/iss/utils/JsonOperations.java
+++ src/main/java/iss/utils/JsonOperations.java
@@ -1,20 +1,24 @@
 package iss.utils;
 
-import iss.model.Astronaut;
-import iss.model.ISSLocation;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import iss.model.ISSLocation;
+import iss.model.SpaceCrew;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 
 public class JsonOperations {
     private final static String BASE_URL = "http://api.open-notify.org/iss-now.json";
     private final static String PEOPLE_URL = "http://api.open-notify.org/astros.json";
 
 
-    private JsonNode jsonDeserialization1(URL url) throws IOException {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.asList(getSpaceCrew()));
+    }
+    private static JsonNode jsonDeserialization1(URL url) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readTree(url);
     }
@@ -23,7 +27,6 @@ public class JsonOperations {
         JsonNode request;
         try {
             request = jsonDeserialization1(new URL(BASE_URL));
-            System.out.println(request.toString());
             return new ISSLocation(request.get("timestamp").asInt(),
                     request.get("iss_position").get("latitude").asDouble(),
                     request.get("iss_position").get("longitude").asDouble());
@@ -32,12 +35,11 @@ public class JsonOperations {
         }
     }
 
-    public Astronaut[] getAstronauts() throws IOException {
+    public static SpaceCrew[] getSpaceCrew() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         JsonNode request = jsonDeserialization1(new URL(PEOPLE_URL));
         String json = request.get("people").toString();
-        System.out.println(json);
-        return objectMapper.readValue(json, Astronaut[].class);
+        return objectMapper.readValue(json, SpaceCrew[].class);
     }
 }
